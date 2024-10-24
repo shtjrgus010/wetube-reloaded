e2c2b37f6e4d9da01db8668f4249cec69bc09b61
노석현 <shtjrgus010@github.com>
2024-10-24 16:58:59 +0900

    feat: Write template

--- README.md
+++ README.md
@@ -26,3 +26,44 @@
 정규식을 사용(\\d+)해 숫자만 허용
 
 nodeJs의 경우 :id가 코드 줄에서 상위에 위치하면 id가 포함하지 않는 경로에도 영향을 미치기 때문에 하위에 위치
+
+## 2024.10.24
+
+### Write Template
+
+1. Install pug
+
+- `npm install pug`로 Pug 설치
+
+2. Configure Express
+
+- `app.set("views", process.cwd() + "/src/views")`을 사용해 Express에서 뷰 엔진으로 Pug 설정
+- 뷰 경로는 `process.cwd() + '/views'` 로 설정
+
+3. Writing and Rendering Pug Templates
+
+- Pug 파일을 **소문자**로 작성
+- `res.render("home")`으로 Pug 파일을 렌더링해 HTML로 변환
+
+4. Template Inheritance
+
+- `extends`로 베이스 템플릿(base.pug)을 확장
+- `block`을 사용해 확장된 템플릿에서 특정 부분을 채워넣음
+
+5. Iteration and Array Handling
+
+- `each` 루프를 통해 `videos` 배열을 순회
+
+6. Use Mixin
+
+- `mixin` 을 정의하여 동일한 **HTML 구조**를 반복적으로 생성
+- `mixin video(video)`는 비디오 정보를 받아 각 비디오 속성 표시
+
+```pug
+mixin video(video)
+    div
+        h4=video.title
+    ul
+        li #{video.rating}/5
+        li #{video.comments} comments.
+```
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,35 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch Video #${req.params.id}`);
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 3,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => res.send("Edit");
+export const see = (req, res) => res.render("watch");
+
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("Upload video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
